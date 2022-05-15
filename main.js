a92954186358b897bd7fc2410e6adfcae0058277
chrism-48 <moore4020@gmail.com>
2022-05-15 11:58:43 -0400

    added play pause animation

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+let myContainer1 = document.querySelector('.container1');
+let myPlayer1 = document.querySelector('.player1');
+let myBullet1 = document.querySelector('.bullet1');
+
+
+
+let startAction = anime ({
+        targets: myBullet1,
+            translateY: [-21, 530],       
+            loop:true,
+            direction:'alternate',
+            rotate: '360',
+            easing: 'easeInOutSine',
+            autoplay:false,       
+});
+
+
+
+function runAnimation() {
+    startAction.play();
+}
+
+function stopAnimation() {
+    startAction.pause();
+}
+
+
+let startMove = document.querySelector('.btn1');
+
+startMove.addEventListener('click', runAnimation);
+
+let stopMove = document.querySelector('.btn2');
+
+stopMove.addEventListener('click', stopAnimation);
+
